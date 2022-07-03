10a4ee5a5811b9061530e119b08dfa73a1a3d12b
shambhavi082001 <shambhavi9192@gmail.com>
2022-07-03 18:40:30 +0530

    streamlit

--- classes/multimedia/adding_ext_over_video.py
+++ classes/multimedia/adding_ext_over_video.py
@@ -8,7 +8,7 @@ while True:
     print(img.shape)
     if state:
         cv2.rectangle(img,(0,0),(260,40),(0,255,0),-1)
-        cv2.putText(img,"Webcam output", (10,30), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255),2)
+        cv2.putText(img,"Webcam output", (10,40), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255),2)
         cv2.imshow("video output",img)
     
         if cv2.waitKey(1) == 27:
